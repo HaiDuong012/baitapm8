30f338e85f95c770d0513d358d5de18c2e1d8aea
HaiDuong012 <150207115+HaiDuong012@users.noreply.github.com>
2023-12-13 08:04:31 +0700

    Add files via upload

---
+++ tn.js
@@ -0,0 +1,19 @@
+let tn1 = document.querySelector("#tnso1");
+console.log(tn1);
+
+// let array = [];
+
+// const handleAddInput = (a, b) => {
+//   a.push(b);
+// };
+
+// let loadData = () => {
+//   console.log(array);
+// };
+
+tnso1.addEventListener("input", (e) => {
+  // const inputtn1 = tn1.value;
+  // handleAddInput(array, inputtn1);
+  // loadData();
+  console.log(e.target.value);
+});
